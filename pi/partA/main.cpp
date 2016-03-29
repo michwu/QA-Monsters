4f2ebe1cca3e818d7d03481d53472156afe8a247
Jonathan Hoi Fung Lai <jonathan.lai@live.com>
2016-03-28 21:26:51 -0400

    fixed compile errors

--- pi/partA/main.cpp
+++ pi/partA/main.cpp
@@ -1,8 +1,12 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <unordered_set>
+#include <set>
 #include <vector>
+#include <map>
+#include <tuple>
+#include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
@@ -13,20 +17,20 @@ int main(int argc, const char* argv[]) {
   int support = 3;
   int confidence = 65;
 
-  map<string, unordered_set<size_t>*> htMap;							// a map of function names to sets of integer hashes of functions that are in the function
+  map<string, set<size_t>*> htMap;							// a map of function names to sets of integer hashes of functions that are in the function
   map<size_t, string> hashToFuncName;											// maps integer hashes to function names
-  unordered_set<pair<size_t, size_t>>	testPairs;					// pairs to test
+  set<pair<size_t, size_t> >	testPairs;					// pairs to test
 
   if (argc == 3) {
     support = atoi(argv[1]);
-    confidence = atoi(argv[2]);	
+    confidence = atoi(argv[2]);
   }
 
   string curFunc;
   hash<string> str_hash;
   for (string line; getline(cin, line);) {
-    if (line.find(CALLER_STR) != line.end()) {
-      unordered_set<size_t> *callees = new unordered_set<size_t>();
+    if (line.find(CALLER_STR) != string::npos) {
+      set<size_t> *callees = new set<size_t>();
 
       int endIndex = line.find("'", CALLER_STR.length());
       curFunc = line.substr(CALLER_STR.length(), endIndex-CALLER_STR.length());
@@ -34,19 +38,19 @@ int main(int argc, const char* argv[]) {
       hashToFuncName[str_hash(curFunc)] = curFunc;
     }
 
-		if (line.find(CALLEE_STR) != line.end()) {
-      std::unordered_set<size_t> *callees = htMap[curFunc];
+		if (line.find(CALLEE_STR) != string::npos) {
+      std::set<size_t> *callees = htMap[curFunc];
 
-      string callee = line.substr(CALLEE_STR.length(), line.length()-CALLEE_STR.length()); 
+      string callee = line.substr(CALLEE_STR.length(), line.length()-CALLEE_STR.length());
       size_t calleeHash = str_hash(callee);
       callees->insert(calleeHash);
       hashToFuncName[calleeHash] = callee;
 
-      std::string cmpStr1, cmpStr2;
+      std::string curFuncUpper, calleeUpper;
       curFuncUpper.resize(curFunc.length());
       calleeUpper.resize(callee.length());
-      std::transform(curFunc.begin(), curFunc.end(), curFuncUpper.begin(), toupper);
-      std::transform(callee.begin(), callee.end(), calleeUpper.begin(), toupper);
+      std::transform(curFunc.begin(), curFunc.end(), curFuncUpper.begin(), ::toupper);
+      std::transform(callee.begin(), callee.end(), calleeUpper.begin(), ::toupper);
       if (curFuncUpper < calleeUpper) {
         testPairs.insert(make_pair(str_hash(curFuncUpper), str_hash(calleeUpper)));
       } else {
@@ -65,7 +69,7 @@ int main(int argc, const char* argv[]) {
 
     for (auto mapPair : htMap) {
       string funcName = mapPair.first;
-      unordered_set<size_t> *functionSet = mapPair.second;
+      set<size_t> *functionSet = mapPair.second;
 
       bool aFound = (functionSet->find(a) != functionSet->end());
       bool bFound = (functionSet->find(b) != functionSet->end());
@@ -83,17 +87,17 @@ int main(int argc, const char* argv[]) {
       }
     }
 
-    double confidenceA = (((double)supportAB)/((double)supportA))) * 100.0;
+    double confidenceA = (((double)supportAB)/((double)supportA)) * 100.0;
     if(supportAB >= support && confidenceA >= (double)confidence) {
       for (string funcName : aOnlyList) {
-    		cout << "bug: " << hashToFuncName(a) << "A in " << funcName << ", pair: (" << hashToFuncName(a) << ", " << hashToFuncName(b) << "), support: " << supportAB << ", confidence: " << setprecision(9) << confidenceA << "%" << endl;
+    		cout << "bug: " << hashToFuncName[a] << "A in " << funcName << ", pair: (" << hashToFuncName[a] << ", " << hashToFuncName[b] << "), support: " << supportAB << ", confidence: " << setprecision(9) << confidenceA << "%" << endl;
       }
     }
-    
-    double confidenceB = (((double)supportAB)/((double)supportB))) * 100.0;
+
+    double confidenceB = (((double)supportAB)/((double)supportB)) * 100.0;
     if(supportAB >= support && confidenceB >= (double)confidence) {
       for (string funcName : bOnlyList) {
-    		cout << "bug: " << hashToFuncName(b) << "A in " << funcName << ", pair: (" << hashToFuncName(a) << ", " << hashToFuncName(b) << "), support: " << supportAB << ", confidence: " << setprecision(9) << confidenceB << "%" << endl;
+    		cout << "bug: " << hashToFuncName[b] << "A in " << funcName << ", pair: (" << hashToFuncName[a] << ", " << hashToFuncName[b] << "), support: " << supportAB << ", confidence: " << setprecision(9) << confidenceB << "%" << endl;
       }
     }
   }
